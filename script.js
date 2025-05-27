46b82e472ee2c74b8c595c63f20bac8d7cf9c98b
cevikm33 <mcevik1998@hotmail.com>
2025-05-27 21:03:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* script.js */
+document.addEventListener("DOMContentLoaded", function() {
+  const envelope = document.querySelector(".envelope");
+  envelope.addEventListener("click", function() {
+    envelope.classList.toggle("open");
+  });
+});
